8a554ce457f8858a2206b57f0383de0c8893a002
Alexander Biryukov <sanya.rnd@gmail.com>
2019-05-19 23:34:32 +0300

    Added package-info.java

---
+++ src/main/java/io/github/sanyarnd/applocker/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides file-based locking mechanism with inter-process communication support.
+ *
+ * @author Alexander Biryukov
+ */
+@ParametersAreNonnullByDefault
+package io.github.sanyarnd.applocker;
+
+import javax.annotation.ParametersAreNonnullByDefault;
